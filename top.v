98f37ce5861770892d6576a915bc2232f02865aa
TRISTINWATKINS <156130938+TRISTINWATKINS@users.noreply.github.com>
2025-02-27 12:59:20 -0700

    Update top.v

--- top.v
+++ top.v
@@ -1,54 +1,38 @@
 // Implement top level module
 module top(
-    input sw0,  
-    input sw1,  
-    
-    input sw2,  
-    input sw3,  
-    
-    input sw4,  
-    input sw5,  
-    input sw6,  
-    input sw7,  
-    
-    output led0, 
-    
-    output led1, 
-    output led2, 
-    
-    output led3, 
-    output led4, 
-    output led5  
+    input [7:0]sw,  
+    output [5:0]led 
 );
 
     light light_inst(
-        .downstairs(sw0),
-        .upstairs(sw1),
-        .stair_light(led0)
+        .downstairs(sw[0]),
+        .upstairs(sw[1]),
+        .stair_light(led[0])
     );
 
     adder adder_inst(
-        .A(sw2),
-        .B(sw3),
-        .Y(led1),
-        .Carry(led2)
+        .A(sw[2]),
+        .B(sw[3]),
+        .Y(led[1]),
+        .Carry(led[2])
     );
 
+    wire carry;
     
     full_adder full_adder_inst0(
-        .A(sw4),        
-        .B(sw6),        
+        .A(sw[4]),        
+        .B(sw[6]),        
         .Cin(0),        
-        .Y(led3),       
-        .Cout(carry0)   
+        .Y(led[3]),       
+        .Cout(carry)   
     );
 
     full_adder full_adder_inst1(
-        .A(sw5),        
-        .B(sw7),        
-        .Cin(carry0),  
-        .Y(led4),      
-        .Cout(led5)     
+        .A(sw[5]),        
+        .B(sw[7]),        
+        .Cin(carry),  
+        .Y(led[4]),      
+        .Cout(led[5])     
     );
 
 endmodule
